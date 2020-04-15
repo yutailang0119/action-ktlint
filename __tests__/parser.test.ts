2d575606193f291c9d65fda7ceb2ff5a2cf826a1
yutailang0119 <muta.yutaro@gmail.com>
2020-04-15 10:55:24 +0900

    Fix parser.test to add test for multiple annotations in one file.

--- __tests__/parser.test.ts
+++ __tests__/parser.test.ts
@@ -6,20 +6,32 @@ test('test parse', () => {
   <checkstyle version="8.0">
     <file name="Foo.kt">
       <error line="3" column="1" severity="error" message="Unused import" source="no-unused-imports"/>
+      <error line="22" column="1" severity="error" message="Needless blank line(s)" source="no-consecutive-blank-lines"/>
     </file>
     <file name="Bar.kts">
-      <error line="22" column="1" severity="error" message="Needless blank line(s)" source="no-consecutive-blank-lines"/>
+      <error line="11" column="1" severity="error" message="Needless blank line(s)" source="no-consecutive-blank-lines"/>
     </file>
   </checkstyle>`
 
   const annotation1 = new Annotation('error', 'Foo.kt', 3, 1, 'Unused import')
   const annotation2 = new Annotation(
     'error',
-    'Bar.kts',
+    'Foo.kt',
     22,
     1,
     'Needless blank line(s)'
   )
+  const annotation3 = new Annotation(
+    'error',
+    'Bar.kts',
+    11,
+    1,
+    'Needless blank line(s)'
+  )
 
-  expect(parseXml(xml)).resolves.toEqual([annotation1, annotation2])
+  expect(parseXml(xml)).resolves.toEqual([
+    annotation1,
+    annotation2,
+    annotation3
+  ])
 })
