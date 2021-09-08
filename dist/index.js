4bfa276db00740ffc084ed2f96068403c92692a7
yutailang0119 <yutailang0119@gmail.com>
2021-09-08 20:21:09 +0900

    Fix to use getBooleanInput

--- dist/index.js
+++ dist/index.js
@@ -99,7 +99,7 @@ async function run() {
     try {
         const xmlPath = core.getInput('xml_path', { required: true });
         const globOptions = {
-            followSymbolicLinks: core.getInput('follow-symbolic-links').toUpperCase() !== 'FALSE'
+            followSymbolicLinks: core.getBooleanInput('follow-symbolic-links')
         };
         const globber = await glob.create(xmlPath, globOptions);
         const files = await globber.glob();
