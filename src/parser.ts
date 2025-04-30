d55aa9e662440c8e2b4b531ee5826c221e6c61f6
yutailang0119 <yutailang0119@gmail.com>
2025-04-30 22:58:11 +0900

    Add src files

---
+++ src/parser.ts
@@ -0,0 +1,59 @@
+import fs from 'fs'
+import * as core from '@actions/core'
+import * as xml2js from 'xml2js'
+import {Annotation} from './annotation'
+
+export const parseXmls = async (
+  files: string[],
+  ignoreWarnings: boolean
+): Promise<Annotation[]> => {
+  const list = await Promise.all(
+    files.map(async file => {
+      const xml = fs.readFileSync(file, 'utf-8')
+      return await parseXml(xml, ignoreWarnings)
+    })
+  )
+  return list.flat()
+}
+
+export const parseXml = async (
+  text: string,
+  ignoreWarnings: boolean
+): Promise<Annotation[]> => {
+  const parser = new xml2js.Parser()
+  const xml = await parser.parseStringPromise(text)
+  if (xml.checkstyle.file === undefined) return []
+  return new Promise(resolve => {
+    try {
+      const annotations: Annotation[] = []
+      for (const fileElement of xml.checkstyle.file) {
+        if (fileElement.error === undefined) continue
+
+        const file = fileElement.$
+        for (const errorElement of fileElement.error) {
+          const error = errorElement.$
+
+          const annotation = new Annotation(
+            error.severity,
+            error.message,
+            file.name,
+            parseInt(error.line),
+            parseInt(error.column)
+          )
+          annotations.push(annotation)
+        }
+      }
+      if (ignoreWarnings === true) {
+        resolve(
+          annotations.filter(annotation => {
+            return annotation.severityLevel !== 'warning'
+          })
+        )
+      } else {
+        resolve(annotations)
+      }
+    } catch (error) {
+      core.debug(`failed to read ${error}`)
+    }
+  })
+}
