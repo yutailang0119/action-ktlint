8c25b7c5c00db41d5a84a3117db76acea901beac
yutailang0119 <yutailang0119@gmail.com>
2025-04-30 23:17:06 +0900

    Fix tests

--- __tests__/parser.test.ts
+++ __tests__/parser.test.ts
@@ -1,95 +1,100 @@
 import path from 'path'
-import {expect, test} from '@jest/globals'
+import url from 'url'
+import {expect} from '@jest/globals'
 import {Annotation} from '../src/annotation.js'
 import {parseXmls, parseXml} from '../src/parser.js'
 
-test('test parseXmls', () => {
-  const file1 = path.join(__dirname, 'resource', 'ktlint-report.xml')
-  const file2 = path.join(__dirname, 'resource', 'empty-report.xml')
+describe('parser.ts', () => {
+  const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
 
-  const annotation1 = new Annotation('error', 'Unused import', 'Foo.kt', 3, 1)
-  const annotation2 = new Annotation(
-    'warning',
-    'Needless blank line(s)',
-    'Foo.kt',
-    22,
-    1
-  )
-  const annotation3 = new Annotation(
-    'warning',
-    'Needless blank line(s)',
-    'Bar.kts',
-    45,
-    1
-  )
+  it('test parseXmls', () => {
+    const file1 = path.join(__dirname, 'resource', 'ktlint-report.xml')
+    const file2 = path.join(__dirname, 'resource', 'empty-report.xml')
 
-  expect(parseXmls([file1, file2], false)).resolves.toEqual([
-    annotation1,
-    annotation2,
-    annotation3
-  ])
-})
+    const annotation1 = new Annotation('error', 'Unused import', 'Foo.kt', 3, 1)
+    const annotation2 = new Annotation(
+      'warning',
+      'Needless blank line(s)',
+      'Foo.kt',
+      22,
+      1
+    )
+    const annotation3 = new Annotation(
+      'warning',
+      'Needless blank line(s)',
+      'Bar.kts',
+      45,
+      1
+    )
 
-test('test parseXmls and ignore warnings', () => {
-  const file1 = path.join(__dirname, 'resource', 'ktlint-report.xml')
-  const file2 = path.join(__dirname, 'resource', 'empty-report.xml')
+    expect(parseXmls([file1, file2], false)).resolves.toEqual([
+      annotation1,
+      annotation2,
+      annotation3
+    ])
+  })
 
-  const annotation1 = new Annotation('error', 'Unused import', 'Foo.kt', 3, 1)
+  it('test parseXmls and ignore warnings', () => {
+    const file1 = path.join(__dirname, 'resource', 'ktlint-report.xml')
+    const file2 = path.join(__dirname, 'resource', 'empty-report.xml')
 
-  expect(parseXmls([file1, file2], true)).resolves.toEqual([annotation1])
-})
+    const annotation1 = new Annotation('error', 'Unused import', 'Foo.kt', 3, 1)
 
-test('test parseXml with error', () => {
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>
-  <checkstyle version="8.0">
-    <file name="Foo.kt">
-      <error line="3" column="1" severity="error" message="Unused import" source="no-unused-imports"/>
-      <error line="22" column="1" severity="warning" message="Needless blank line(s)" source="no-consecutive-blank-lines"/>
-    </file>
-    <file name="Bar.kts">
-      <error line="11" column="1" severity="warning" message="Needless blank line(s)" source="no-consecutive-blank-lines"/>
-    </file>
-  </checkstyle>`
+    expect(parseXmls([file1, file2], true)).resolves.toEqual([annotation1])
+  })
 
-  const annotation1 = new Annotation('error', 'Unused import', 'Foo.kt', 3, 1)
-  const annotation2 = new Annotation(
-    'warning',
-    'Needless blank line(s)',
-    'Foo.kt',
-    22,
-    1
-  )
-  const annotation3 = new Annotation(
-    'warning',
-    'Needless blank line(s)',
-    'Bar.kts',
-    11,
-    1
-  )
+  it('test parseXml with error', () => {
+    const xml = `<?xml version="1.0" encoding="UTF-8"?>
+    <checkstyle version="8.0">
+      <file name="Foo.kt">
+        <error line="3" column="1" severity="error" message="Unused import" source="no-unused-imports"/>
+        <error line="22" column="1" severity="warning" message="Needless blank line(s)" source="no-consecutive-blank-lines"/>
+      </file>
+      <file name="Bar.kts">
+        <error line="11" column="1" severity="warning" message="Needless blank line(s)" source="no-consecutive-blank-lines"/>
+      </file>
+    </checkstyle>`
 
-  expect(parseXml(xml, false)).resolves.toEqual([
-    annotation1,
-    annotation2,
-    annotation3
-  ])
-})
+    const annotation1 = new Annotation('error', 'Unused import', 'Foo.kt', 3, 1)
+    const annotation2 = new Annotation(
+      'warning',
+      'Needless blank line(s)',
+      'Foo.kt',
+      22,
+      1
+    )
+    const annotation3 = new Annotation(
+      'warning',
+      'Needless blank line(s)',
+      'Bar.kts',
+      11,
+      1
+    )
 
-test('test parseXml without file', () => {
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>
-  <checkstyle version="8.0">
-  </checkstyle>`
+    expect(parseXml(xml, false)).resolves.toEqual([
+      annotation1,
+      annotation2,
+      annotation3
+    ])
+  })
 
-  expect(parseXml(xml, false)).resolves.toEqual([])
-})
+  it('test parseXml without file', () => {
+    const xml = `<?xml version="1.0" encoding="UTF-8"?>
+    <checkstyle version="8.0">
+    </checkstyle>`
 
-test('test parseXml without error', () => {
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>
-  <checkstyle version="8.0">
-    <file name="Foo.kt">
-    </file>
-    <file name="Bar.kts">
-    </file>
-  </checkstyle>`
+    expect(parseXml(xml, false)).resolves.toEqual([])
+  })
 
-  expect(parseXml(xml, false)).resolves.toEqual([])
+  it('test parseXml without error', () => {
+    const xml = `<?xml version="1.0" encoding="UTF-8"?>
+    <checkstyle version="8.0">
+      <file name="Foo.kt">
+      </file>
+      <file name="Bar.kts">
+      </file>
+    </checkstyle>`
+
+    expect(parseXml(xml, false)).resolves.toEqual([])
+  })
 })
